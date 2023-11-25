f070b99e96d6779fe5b33c82a69238e1534c7192
Nick-Rodrigues <RM551060@fiap.com.br>
2023-11-24 23:25:07 -0300

    Estrutura inicial rodape.

---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import "./rodape.modules.css";
+
+export default function Rodape() {
+  return (
+    <>
+    <footer className="rodape">
+        <div className="integrantes-main">
+          <h1 className="subtitulo">Integrantes</h1>
+          <div>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
