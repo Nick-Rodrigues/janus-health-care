0880f5b0d3f9bddb2f7fd02845652cc7321b850d
Nick-Rodrigues <RM551060@fiap.com.br>
2023-11-24 23:36:50 -0300

    Etapas pronta.

---
+++ src/app/etapas/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Etapas() {
+    return (
+      <div className="etapas">
+        <h1>Etapas</h1>
+        <h2>Cadastro</h2>
+        <p>Será necessário documentações do cliente para o cadastro</p>
+        <p>Incluindo, se possível o quadro atual do paciente</p>
+        <h2>Consulta</h2>
+        <p>Após se cadastrar ou quando quiser, é só acessar o aplicativo</p>
+        <p>
+          O sistema ira ler seu quadro para possibilitar pré diagnósticos e
+          podendo ser mais acertivo
+        </p>
+      </div>
+    );
+  }
